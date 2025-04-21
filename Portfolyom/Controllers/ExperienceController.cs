c4b169bd17dbeca917c7326447fb01668ee2a354
sseymakulak <seymakulak@posta.mu.edu.tr>
2025-04-21 16:28:48 +0300

    MyPortfolyom

--- Portfolyom/Controllers/ExperienceController.cs
+++ Portfolyom/Controllers/ExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portfolyom.DAL.Context;
+using Portfolyom.DAL.Entities;
 
 namespace Portfolyom.Controllers
 {
@@ -10,6 +11,43 @@ namespace Portfolyom.Controllers
         {
             var values = context.Experiences.ToList();
            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value=context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
---
+++ Portfolyom/Controllers/StatistikController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolyom.Controllers
+{
+    public class StatistikController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
